394c35fbde96c8d962f02e81b18c8d7cb4667718
peterfaul <peter.faul.glass@gmail.com>
2017-06-28 20:16:37 +0200

    Part_1: Done.

--- printing.py
+++ printing.py
@@ -5,26 +5,31 @@ def count_games_print(file_name):
 
     pp = pprint.PrettyPrinter()
     pp.pprint(reports.count_games(file_name))
+    return reports.count_games(file_name)
 
 def decide_print(file_name, year):
 
     pp = pprint.PrettyPrinter()
     pp.pprint(reports.decide(file_name, year))
+    return reports.decide(file_name, year)
 
 def get_latest_print(file_name):
 
     pp = pprint.PrettyPrinter()
     pp.pprint(reports.get_latest(file_name))
+    return reports.get_latest(file_name)
 
 def count_by_genre_print(file_name, genre):
 
     pp = pprint.PrettyPrinter()
     pp.pprint(reports.count_by_genre(file_name, genre))
+    return reports.count_by_genre(file_name, genre)
 
 def get_line_number_by_title_print(file_name, title):
 
     pp = pprint.PrettyPrinter()
     pp.pprint(reports.get_line_number_by_title(file_name, title))
+    return reports.get_line_number_by_title(file_name, title)
 
 def main():
     name_of_the_file = "game_stat.txt"
@@ -37,5 +42,3 @@ def main():
     get_latest_print(name_of_the_file)
     count_by_genre_print(name_of_the_file, genre)
     get_line_number_by_title_print(name_of_the_file, title)
-
-main()
