5f33b7e47e9afce433177f4b601e965bd0907b87
peterfaul <peter.faul.glass@gmail.com>
2017-06-28 19:13:37 +0200

    No comment

--- printing.py
+++ printing.py
@@ -1,2 +1,41 @@
+import pprint
+import reports
 
-# Printing functions
+def count_games_print(file_name):
+
+    pp = pprint.PrettyPrinter()
+    pp.pprint(reports.count_games(file_name))
+
+def decide_print(file_name, year):
+
+    pp = pprint.PrettyPrinter()
+    pp.pprint(reports.decide(file_name, year))
+
+def get_latest_print(file_name):
+
+    pp = pprint.PrettyPrinter()
+    pp.pprint(reports.get_latest(file_name))
+
+def count_by_genre_print(file_name, genre):
+
+    pp = pprint.PrettyPrinter()
+    pp.pprint(reports.count_by_genre(file_name, genre))
+
+def get_line_number_by_title_print(file_name, title):
+
+    pp = pprint.PrettyPrinter()
+    pp.pprint(reports.get_line_number_by_title(file_name, title))
+
+def main():
+    name_of_the_file = "game_stat.txt"
+    year = 2000
+    genre = "RPG"
+    title = "Half-Life"
+
+    count_games_print(name_of_the_file)
+    decide_print(name_of_the_file, year)
+    get_latest_print(name_of_the_file)
+    count_by_genre_print(name_of_the_file, genre)
+    get_line_number_by_title_print(name_of_the_file, title)
+
+main()
--- reports.py
+++ reports.py
@@ -1,2 +1,72 @@
 
-# Report functions
+def count_games(file_name):
+
+    with open(file_name) as file:
+        result = len(file.readlines())
+    return result
+
+# count_games("game_stat.txt")
+
+
+def decide(file_name, year):
+
+    file = open(file_name)
+    year = str(year)
+    file_tuple = tuple(file.read().split())
+    file.close()
+
+    if year not in file_tuple:
+        return False
+    else:
+        return True
+
+# decide("game_stat.txt", 1935)
+
+
+def get_latest(file_name):
+
+    file = file_name
+    title_and_year = {}
+
+    with open(file) as var:
+        for line in var:
+            title_and_year.update({line.split("\t")[0] : line.split("\t")[2]})
+
+    return max(title_and_year, key=title_and_year.get)
+
+# get_latest("game_stat.txt")
+
+
+def count_by_genre(file_name, genre):
+
+    file = file_name
+    list_of_genres = []
+
+    with open(file) as var:
+        for line in var:
+            list_of_genres.append(line.split("\t")[3])
+
+    genre_counter = 0
+    for i in list_of_genres:
+        if i == genre:
+            genre_counter += 1
+
+    return genre_counter
+
+# count_by_genre("game_stat.txt", "Simulation")
+
+
+def get_line_number_by_title(file_name, title):
+
+    file = file_name
+    with open(file) as var:
+        try:
+            for number, line in enumerate(var, 1):
+                if title == line.split("\t")[0]:
+                    return number
+            if title not in file:
+                raise ValueError("%s is not on the list." %title)
+        except ValueError as err:
+            print("FAILURE! %s" % err)
+
+get_line_number_by_title("game_stat.txt", "Half-Life")
